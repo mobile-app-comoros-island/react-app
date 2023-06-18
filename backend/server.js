6f5d00b78e147540cb4455a8d75c1961bbe766f6
Emily Chen <emilyaychen@gmail.com>
2023-06-18 14:07:11 -0400

    connected survey from front to back end

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+app.use(cors()); // Enable CORS for all routes
+// In-memory survey data
+surveyData = {
+  title: 'Feedback Survey',
+  pages: [
+    {
+      name: 'page1',
+      elements: [
+        {
+          type: 'radiogroup',
+          name: 'rating',
+          title: 'Rate your experience:',
+          choices: ['Excellent', 'Good', 'Average', 'Poor'],
+          isRequired: true,
+        },
+        {
+          type: 'comment',
+          name: 'comments',
+          title: 'Additional Comments:',
+        },
+      ],
+    },
+  ],
+};
+
+// API endpoint to fetch survey data
+app.get('/api/surveyData', (req, res) => {
+  res.json(surveyData);
+});
+app.post('/api/submitSurvey', (req, res) => {
+    const submittedData = req.body;
+    surveyData = submittedData;
+  
+    res.sendStatus(200);
+  });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,14 +1,29 @@
 import React, { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-//Hannah's Note: Moved the survey into its own component to make it easier to read
-import SurveyForm from './SurveyForm';
-//import * as Survey from 'survey-react';
-//import 'survey-react/survey.css';
+import * as Survey from 'survey-react';
+import 'survey-react/survey.css';
+import config from './config';
 
 function MapContainer({ lat, lon }) {
   const mapRef = useRef(null);
   const [showSurvey, setShowSurvey] = useState(false);
+  const [surveyData, setSurveyData] = useState(null);
+
+  useEffect(() => {
+    // Fetch survey data from the backend
+    async function fetchSurveyData() {
+      try {
+        const response = await fetch(`${config.backendUrl}/api/surveyData`);
+        const data = await response.json();
+        setSurveyData(data);
+      } catch (error) {
+        console.error('Error fetching survey data:', error);
+      }
+    }
+
+    fetchSurveyData();
+  }, []);
 
   useEffect(() => {
     // Create the map instance
@@ -22,14 +37,12 @@ function MapContainer({ lat, lon }) {
       ],
     });
 
-    // Define a custom marker icon
+    // Create a marker and add it to the map
     const markerIcon = L.icon({
-      iconUrl: process.env.PUBLIC_URL + '/map-marker-icon-600x-map-marker-11562939743ayfahlvygl-removebg-preview.png', // Replace with the URL of your marker image
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
-    });
-
-    // Create a marker with the custom icon and add it to the map
+        iconUrl: process.env.PUBLIC_URL + '/map-marker-icon-600x-map-marker-11562939743ayfahlvygl-removebg-preview.png', // Replace with the URL of your marker image
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+      });
     const marker = L.marker([lat, lon], { icon: markerIcon }).addTo(mapRef.current);
 
     // Handle marker click event
@@ -43,10 +56,38 @@ function MapContainer({ lat, lon }) {
     };
   }, [lat, lon]);
 
+  // Callback function when survey is completed
+  const onCompleteSurvey = (survey) => {
+    // Send survey data to backend for persistence
+    fetch(`${config.backendUrl}/api/submitSurvey`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(survey.data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('Survey data submitted successfully');
+        } else {
+          console.error('Failed to submit survey data');
+        }
+      })
+      .catch((error) => {
+        console.error('Error submitting survey data:', error);
+      });
+  };
+
   return (
     <div className="map-container">
       <div id="map" className="map" style={{ height: '400px' }} />
-      {showSurvey && <SurveyForm />}
+
+      {showSurvey && surveyData && (
+        <div className="survey-container">
+          <h2>Survey Form</h2>
+          <Survey.Survey json={surveyData} onComplete={onCompleteSurvey} />
+        </div>
+      )}
     </div>
   );
 }
